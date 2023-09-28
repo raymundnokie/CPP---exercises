9c36d1e1624fea5c38e92babc75efeaa27ecad63
raymundnokie <146303490+raymundnokie@users.noreply.github.com>
2023-09-28 10:17:19 +0800

    Add files via upload

---
+++ PRACTICE 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string x = "HELLO WORLD!";
+	int x_size = x.size();
+	x[0] = 'J' ;
+	x_size -= 4;
+	cout<< x <<"\n";
+	cout<<x_size <<"\n";
+	cout << x[x_size] <<"\n";
+	
+	
+	
+
+}
